b4a612c2c08ef39105ff0c00cab2141e924e852e
blessings00 <blessings@donorbox.org>
2022-06-07 20:31:04 +0200

    function to handle clicks

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const inpFile = document.getElementById("inpFile")
 const previewContainer = document.getElementById("docPreview")
 const previewDoc = previewContainer.querySelector(".doc-preview__doc")
 const previewDefaultText = previewContainer.querySelector(".doc-preview__default-text")
-
+var firstClick = 0;
+var secondClick = 0;
+var firstClickX = 0;
+var firstClickY = 0;
+var secondClickX = 0;
+var secondClickY = 0;
 
 inpFile.addEventListener("change", function() {
   const file = this.files[0];
@@ -19,66 +24,85 @@ inpFile.addEventListener("change", function() {
   }
 });
 
- function showCoords(event) {
-  var x = event.clientX;
-  var y = event.clientY;
-  var coor = "X coords: " + x + ", Y coords: " + y;
-  document.getElementById("coordinates").innerHTML = coor;
-  document.getElementById("coordinatex").innerHTML = x;
-  document.getElementById("coordinatey").innerHTML = y
-}
-
-function clearCoor() {
-  document.getElementById("coordinates").innerHTML = "";
-}
-
-
-function saveCoor() {
-  document.addEventListener("click", function() {
-    var x = document.getElementById("coordinatex").innerHTML;
-    var y = document.getElementById("coordinatey").innerHTML;
-    var name = document.getElementById("inpFile").value;
-    var json = {
-      "name": name,
-      "x": x,
-      "y": y
-    }
-    var jsonString = JSON.stringify(json);
-    var fs = require('fs');
-    fs.writeFile("coords.json", jsonString, function(err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("The file was saved!");
-    });
-  })
-}
-
-
-function altClick() {
-  document.addEventListener("click", function() {
-    var x = document.getElementById("coordinatex").innerHTML;
-    var y = document.getElementById("coordinatey").innerHTML;
-    var width = document.getElementById("coordinatex").innerHTML - document.getElementById("coordinatex").innerHTML;
-    var height = document.getElementById("coordinatey").innerHTML - document.getElementById("coordinatey").innerHTML;
-    var name = document.getElementById("inpFile").value;
-    var json = {
-      "name": name,
-      "x": x,
-      "y": y,
-      "width": width,
-      "height": height
-    }
-    var jsonString = JSON.stringify(json);
-    var fs = require('fs');
-    fs.writeFile("coords.json", jsonString, function(err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("The file was saved!");
-    });
+//The purpose of this function is to:
+//1. Get the coordinates of the mouse click.
+// handle click function 
+// if shift key is pressed save second set of coordinates
+// if shift is not pressed save first set of coordinates
+function handleClick(event) {
+  if (event.shiftKey) {
+    event.offsetX = [];
+    event.offsetY = [];
+    firstClickX = event.offsetX[0];
+    secondClickX = event.offsetY[1];
+    firstClickY = event.offsetY[0];
+    secondClickY = event.offsetY[1];
+  } else {
+    firstClickX = event.offsetX;
+    firstClickY = event.offsetY;
   }
+}
+
+//function get global coordinates returns 2 set of coordinates
+function getGlobalCoordinates(handleClick) {
+  return [firstClickX, firstClickY, secondClickX, secondClickY];
+}
+  
+// function calculates width and height, given 2 sets of coordinates
+// it calculates the distance between the two points as width and height
+// inputs: x1,y1,x2,y2
+// outputs: width, height
+function calculateWidthAndHeight (getGlobalCoordinates) {
+  var x1 = getGlobalCoordinates[0];
+  var y1 = getGlobalCoordinates[1];
+  var x2 = getGlobalCoordinates[2];
+  var y2 = getGlobalCoordinates[3];
+  var width = x2 - x1;
+  var height = y2 - y1;
+  return [width, height];
+}
+
+
+// function button to save coordinates, width and height to a json file
+// takes first set of coordinates, second set, width and height
+// and the value of the "field_name" input field and stores the data
+// in the in-memory-array called "coorArray"
+function
+
+
+//showjson the purpose of this function is to show a popup with a 
+// textarea where we can copy the json data from the coordArray in-memory array
+// the popup has a close button
 
 
 
+
+function showCoords(event) {
+  //1
+  var x = event.offsetX;
+  var y = event.offsetY;
+  document.getElementById("x").innerHTML = x;
+  document.getElementById("y").innerHTML = y;
+
+
+  //2
+  var height = y.firstClick - y.secondClick;
+  var width = x.firstClick - x.secondClick;
+
+
+  document.getElementById("h").innerHTML = height;
+  document.getElementById("w").innerHTML = width;
+
+  // addField
+  var coor = {"height": height, "width": width, "x": x, "y": y};
+  var coorArray = [];
+  coorArray.push(coor);
+
+  // showjson
+  // var coorJSON = JSON.stringify(coorArray);
+  // var blob = new Blob([coorJSON], {type: "application/json"});
+  // var url = URL.createObjectURL(blob);
+  // document.getElementById("download").href = url;
+  // document.getElementById("download").download = "coordinates.json";
+}
 
